60e10b334cf8aec1524e5c56506b5e89270fa253
Umedzhon Abdumuminov <ovegug@gmail.com>
2019-04-09 01:51:06 +0300

    Added .clang-format based on LLVM, removed CMakeLists.txt, style alignment for upscaler element

--- gst/elements/upscaler/gstupscaler.h
+++ gst/elements/upscaler/gstupscaler.h
@@ -12,35 +12,28 @@
 G_BEGIN_DECLS
 
 /* #defines don't like whitespacey bits */
-#define GST_TYPE_UPSCALER \
-  (gst_up_scaler_get_type())
-#define GST_UPSCALER(obj) \
-  (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_UPSCALER,GstUpScaler))
-#define GST_UPSCALER_CLASS(klass) \
-  (G_TYPE_CHECK_CLASS_CAST((klass),GST_TYPE_UPSCALER,GstUpScalerClass))
-#define GST_IS_UPSCALER(obj) \
-  (G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_UPSCALER))
-#define GST_IS_UPSCALER_CLASS(klass) \
-  (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_UPSCALER))
+#define GST_TYPE_UPSCALER (gst_up_scaler_get_type())
+#define GST_UPSCALER(obj) (G_TYPE_CHECK_INSTANCE_CAST((obj), GST_TYPE_UPSCALER, GstUpScaler))
+#define GST_UPSCALER_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST((klass), GST_TYPE_UPSCALER, GstUpScalerClass))
+#define GST_IS_UPSCALER(obj) (G_TYPE_CHECK_INSTANCE_TYPE((obj), GST_TYPE_UPSCALER))
+#define GST_IS_UPSCALER_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), GST_TYPE_UPSCALER))
 
-typedef struct _GstUpScaler      GstUpScaler;
+typedef struct _GstUpScaler GstUpScaler;
 typedef struct _GstUpScalerClass GstUpScalerClass;
 
-struct _GstUpScaler
-{
-  GstElement element;
+struct _GstUpScaler {
+    GstElement element;
 
-  GstPad *sinkpad, *srcpad;
+    GstPad *sinkpad, *srcpad;
 
-  gboolean silent;
+    gboolean silent;
 };
 
-struct _GstUpScalerClass
-{
-  GstElementClass parent_class;
+struct _GstUpScalerClass {
+    GstElementClass parent_class;
 };
 
-GType gst_up_scaler_get_type (void);
+GType gst_up_scaler_get_type(void);
 
 G_END_DECLS
 
