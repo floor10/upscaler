03ae6a9977298e264f62e48232e17d5da04c8bdc
Fadeev, Alexey <alexey.fadeev@intel.com>
2019-05-03 16:44:08 +0300

    Added folder with sample

---
+++ samples/super-resolution.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+VIDEO_FILE=${1:-/root/video-examples/Pexels_Videos_4786_960x540.mp4}
+
+gst-launch-1.0 filesrc location=${VIDEO_FILE} ! decodebin ! video/x-raw ! videoconvert ! interpolator ! upscaler ! videoconvert ! fpsdisplaysink sync=false
