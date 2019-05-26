4edf41cd6c8b5fd698de250918114a28cb48166b
Fadeev, Alexey <alexey.fadeev@intel.com>
2019-05-26 19:04:11 +0300

    Fix locale problem

--- samples/super-resolution.sh
+++ samples/super-resolution.sh
@@ -2,5 +2,7 @@
 
 VIDEO_FILE=${2:-/root/video-examples/Pexels_Videos_4786_960x540.mp4}
 
-gdb --args \
+export LC_NUMERIC="C"
+echo C locale is set
+
 gst-launch-1.0 filesrc location=${VIDEO_FILE} ! decodebin ! video/x-raw ! videoconvert ! interpolator ! upscaler model=${1} ! videoconvert ! fpsdisplaysink sync=false
